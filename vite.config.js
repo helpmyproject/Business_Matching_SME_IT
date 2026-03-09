8af8828946be0016892d2e8dedaa22971cb77db6
Natdanai Chaothai <Natdanai.Chaot@kkumail.com>
2026-03-10 01:47:05 +0700

    index_update_page02

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Business_Matching_SME_IT/',
+  base: 'Business_Matching_SME_IT',
 })
